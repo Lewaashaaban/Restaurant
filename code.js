30f76ea6cf77568a9c5cef0002511af9031fed33
Lewaashaaban <shaabanlewaa@gmail.com>
2023-02-17 20:04:04 +0200

    git push

--- code.js
+++ code.js
@@ -21,32 +21,9 @@ function orderonline() {
 }
 
 
-const selectedElement = document.querySelector("#select");
-const selectedQuantity = document.getElementById("qty");
-const submit = document.getElementById("submit");
 
 
 
 
 
-submit.addEventListener("click", function (event) {
-  event.preventDefault();
-  const selectedsize = selectedElement.value;
-  const quantityvalue = selectedQuantity.value;
-  const selectedNumber = parseInt(quantityvalue);
-
-  let result = 0;
-
-  if (selectedsize === 'option1') {
-    result = selectedNumber * 9;
-  }
-  else if (selectedsize === 'option2') {
-    result = selectedNumber * 13;
-  }
-  else if (selectedsize === 'option3') {
-    result = selectedNumber * 15;
-  }
-  document.getElementById("price").innerHTML = "result is " + result + "$";
-
-})
 
